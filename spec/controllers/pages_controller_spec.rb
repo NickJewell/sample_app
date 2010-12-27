7ca775560889f96c87c7735166504997938f25b4
Nick Jewell <nick.jewell@gmail.com>
2010-12-27 08:23:59 +0000

    Added a site layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -14,6 +14,11 @@ render_views
       response.should have_selector("title",
             :content => "Ruby on Rails Tutorial Sample App | Home")
     end
+    
+    it  "should have a non-blank body" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
   end 
 
   describe "GET 'contact'" do
@@ -21,12 +26,22 @@ render_views
       get 'contact'
       response.should be_success
     end
+    it  "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+            :content => "Ruby on Rails Tutorial Sample App | Contact")
+    end
   end
   
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
+    end
+    it  "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+            :content => "Ruby on Rails Tutorial Sample App | About")
     end
   end
 
